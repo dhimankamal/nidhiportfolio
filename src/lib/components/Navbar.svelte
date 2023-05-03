187ab20e718ba7d6e97695ec9578e6fdb7609d36
kamal <dhimank079@gmail.com>
2023-05-03 22:04:24 +0530

    setup project

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,13 @@
+<nav class="py-6">
+	<div class="container mx-auto flex justify-between">
+		<div>Logo</div>
+		<div class="flex gap-4">
+			<a href="/">Home</a>
+			<a href="/">About</a>
+			<a href="/">Experiance</a>
+		</div>
+		<div>
+			<a href="/">Contact us</a>
+		</div>
+	</div>
+</nav>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div class="grid grid-cols-12 container mx-auto">
+	<div class="col-span-6 bg-gray-100 h-96">test</div>
+	<div>
+		<h1>Nidhi</h1>
+	</div>
+</div>
