b92a60ef52fc04577939b88ee6429307a3eb9d4e
kamal <dhimank079@gmail.com>
2023-05-04 08:00:07 +0530

    add detail doctor inprogress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,310 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<div class="grid grid-cols-12 container mx-auto">
-	<div class="col-span-6 bg-gray-100 h-96">test</div>
-	<div>
-		<h1>Nidhi</h1>
+<section class="text-gray-600 body-font">
+	<div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+		<div
+			class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center"
+		>
+			<h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+				Dr. Nidhi Sharma
+			</h1>
+			<p class="title-font sm:text-xl text-2xl mb-4 font-medium text-gray-900">
+				- Experienced and Compassionate Gynecologist and Obstetrician
+			</p>
+			<p class="mb-8 leading-relaxed">
+				Welcome to the website of Dr. Sharma, a highly skilled and experienced gynecologist and
+				obstetrician who is dedicated to providing the highest quality care to her patients. With
+				extensive experience in ultrasound, obstetric and gynecological surgery, and assisted
+				reproduction, Dr. Sharma has helped countless women achieve optimal reproductive health and
+				overcome a wide range of gynecological and obstetric issues.
+			</p>
+			<div class="flex justify-center">
+				<button
+					class="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg"
+					>Contact</button
+				>
+				<button
+					class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg"
+					>About</button
+				>
+			</div>
+		</div>
+		<div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+			<img
+				class="object-cover object-center rounded"
+				alt="hero"
+				src="https://dummyimage.com/720x600"
+			/>
+		</div>
 	</div>
-</div>
+</section>
+
+<section class="text-gray-600 body-font bg-slate-50">
+	<div class="container px-5 py-24 mx-auto">
+		<div class="flex flex-col text-center w-full mb-20 space-y-4">
+			<h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">About us</h1>
+			<p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+				At Jindal IVF and Sant Memorial Nursing Home, Dr. Nidhi Sharma is a senior consultant in
+				Obstetrics and Gynecology as well as an infertility specialist. Dr. Sharma has extensive
+				experience in assisted reproductive technology, laparoscopic surgeries, obstetric and
+				gynecological ultrasound, and gynecological surgeries such as myomectomy, exploratory
+				laparotomy, vaginal and uterine prolapse operations, and reconstructive tubal, uterine,
+				ovarian, and vaginal surgery.
+			</p>
+			<p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+				Dr. Sharma received her medical degree with distinctions in Pharmacology, Biochemistry,
+				Microbiology, and a certificate of honor with first position in Medicine from the Tamil Nadu
+				NGR Medical University in Chennai. She then pursued her M.S in Obstetrics and Gynecology at
+				NSCB Medical Collage Jabalpur (M.P) and completed her senior residency in the Department of
+				Obstetrics and Gynecology at PGIMER Chandigarh. She is a life member of the Federation of
+				Obs & Gynae Society India, Indian Fertility Society, Chandigarh Obs & Gynae Society, and
+				Indian Society of Assisted Reproduction.
+			</p>
+			<p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+				Dr. Sharma has a keen interest in academics and has presented numerous papers in regional
+				and national conferences. She has contributed various chapters on ART in multiple books and
+				has been an invited faculty at various regional and national conferences. Dr. Sharma has
+				given many talks on different topics related to infertility.
+			</p>
+			<p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+				In addition, Dr. Sharma completed her training in Infertility & IVF for two years at Jindal
+				IVF & Sant Memorial Nursing Home, an ISAR Accredited ART Centre. She also completed her
+				training in module in 3-D ultrasound from GE Healthcare. Dr. Sharma has performed and
+				assisted in various major & minor gynecologic, obstetric, and infertility related
+				procedures.
+			</p>
+			<p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+				Dr. Sharma has completed a thesis on "Use of Low Dose Magnesium Therapy in treatment of
+				Ecclampsia" during her post-graduation. She has presented papers on Programmed Labour,
+				Stimulation protocol for IUI in patients with Hypogonadotropic Hypogonadism, and "Efficacy
+				and safety of addition of low dose human chorionic gonadotropin as luteal support in
+				prevention of OHSS in PCOS patients where GnRh agonist trigger is given for inducing
+				ovulation" at various conferences. Dr. Sharma has also participated in workshops and
+				conferences on endoscopy, birth defects, ART, and advances in infertility management.
+			</p>
+			<p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+				As a joint organizing secretary of various conferences like Fertivision 2008, 7th, 8th, 9th,
+				10th ART update organized annually by Jindal IVF & Sant Memorial Nursing Home under the
+				aegis of IFS, Dr. Sharma has contributed significantly to the dissemination of knowledge in
+				the field of infertility. She is a teaching faculty for IFS fellows and has adequate
+				exposure to various semen preparation techniques, techniques of IVF/ICSI, cryopreservation
+				of semen and embryo, and quality control in the laboratory.
+			</p>
+			<p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+				Overall, Dr. Nidhi Sharma brings a wealth of experience and knowledge to her role as a
+				senior consultant in Obstetrics and Gynecology and infertility specialist. Her commitment to
+				academics and research ensures that she remains up-to-date with the latest advances in the
+				field, while her expertise in all types of infertility related diagnostic and laparoscopic
+				surgeries makes her a sought-after specialist in the field of reproductive medicine.
+			</p>
+		</div>
+		<div class="flex flex-wrap -m-4 text-center">
+			<div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+				<div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+					<svg
+						fill="none"
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						class="text-blue-500 w-12 h-12 mb-3 inline-block"
+						viewBox="0 0 24 24"
+					>
+						<path d="M8 17l4 4 4-4m-4-5v9" />
+						<path d="M20.88 18.09A5 5 0 0018 9h-1.26A8 8 0 103 16.29" />
+					</svg>
+					<h2 class="title-font font-medium text-3xl text-gray-900">2.7K</h2>
+					<p class="leading-relaxed">Downloads</p>
+				</div>
+			</div>
+			<div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+				<div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+					<svg
+						fill="none"
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						class="text-blue-500 w-12 h-12 mb-3 inline-block"
+						viewBox="0 0 24 24"
+					>
+						<path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+						<circle cx="9" cy="7" r="4" />
+						<path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75" />
+					</svg>
+					<h2 class="title-font font-medium text-3xl text-gray-900">1.3K</h2>
+					<p class="leading-relaxed">Users</p>
+				</div>
+			</div>
+			<div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+				<div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+					<svg
+						fill="none"
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						class="text-blue-500 w-12 h-12 mb-3 inline-block"
+						viewBox="0 0 24 24"
+					>
+						<path d="M3 18v-6a9 9 0 0118 0v6" />
+						<path
+							d="M21 19a2 2 0 01-2 2h-1a2 2 0 01-2-2v-3a2 2 0 012-2h3zM3 19a2 2 0 002 2h1a2 2 0 002-2v-3a2 2 0 00-2-2H3z"
+						/>
+					</svg>
+					<h2 class="title-font font-medium text-3xl text-gray-900">74</h2>
+					<p class="leading-relaxed">Files</p>
+				</div>
+			</div>
+			<div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+				<div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+					<svg
+						fill="none"
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						class="text-blue-500 w-12 h-12 mb-3 inline-block"
+						viewBox="0 0 24 24"
+					>
+						<path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
+					</svg>
+					<h2 class="title-font font-medium text-3xl text-gray-900">46</h2>
+					<p class="leading-relaxed">Places</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="text-gray-600 body-font">
+	<div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+		<div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+			<img
+				class="object-cover object-center rounded"
+				alt="hero"
+				src="https://dummyimage.com/720x600"
+			/>
+		</div>
+		<div
+			class="space-y-4 lg:flex-grow md:w-1/2 lg:pr-24 md:pl-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center"
+		>
+			<h2 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Experience</h2>
+
+			<p class=" leading-relaxed">
+				Dr. Sharma has extensive experience in the field of obstetrics, gynecology, and infertility.
+				She has completed her training in infertility and IVF for two years at an ISAR Accredited
+				ART Centre, Jindal IVF & Sant Memorial Nursing Home. She also completed a module in 3-D
+				ultrasound from GE Healthcare.
+			</p>
+			<p class=" leading-relaxed">
+				Throughout her career, Dr. Sharma has performed and assisted in various major and minor
+				gynecologic, obstetric, and infertility-related procedures. These procedures include
+				abdominal and vaginal hysterectomy, exploratory laparotomy, operations for vaginal and
+				uterine prolapse, myomectomy, reconstructive tubal, uterine, ovarian and vaginal surgery,
+				repair of genital fistulae, tubal sterilization by vaginal and abdominal approach, operative
+				and diagnostic laparoscopy and hysteroscopy.
+			</p>
+			<p class="leading-relaxed">
+				Dr. Sharma has also acquired adequate experience in all types of obstetric and gynecological
+				ultrasounds, both transabdominally and transvaginally. She has performed over 1000
+				procedures of transvaginal ultrasound-guided follicular retrievals and embryo transfers and
+				over 2000 procedures of artificial insemination of semen.
+			</p>
+			<p class="leading-relaxed">
+				In addition to her clinical practice, Dr. Sharma is a teaching faculty for IFS fellows. She
+				has also gained adequate exposure to various semen preparation techniques, techniques of
+				IVF/ICSI, cryopreservation of semen and embryo, and quality control in the laboratory.
+			</p>
+			<p class="leading-relaxed">
+				Dr. Sharma has also performed and assisted in all types of infertility-related diagnostic
+				and operative laparoscopic surgeries, including myomectomy, endometriotic adhesiolysis,
+				ovarian cyst removal, tubal cannulations, and more. She has also performed and assisted in
+				various hysteroscopic procedures, such as septal resection, polypectomy, metroplasty,
+				adhesiolysis, and more.
+			</p>
+			<p class="leading-relaxed">
+				Overall, Dr. Sharma's extensive experience and expertise in the field of obstetrics,
+				gynecology, and infertility make her a highly competent and reliable healthcare
+				professional.
+			</p>
+		</div>
+	</div>
+</section>
+
+<section class="text-gray-600 body-font">
+	<div class="container px-5 py-24 mx-auto flex flex-wrap">
+	  <div class="flex flex-wrap -m-4">
+		<div class="p-4 md:w-full">
+		  <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
+			<!-- <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-blue-100 text-blue-500 flex-shrink-0">
+			  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-8 h-8" viewBox="0 0 24 24">
+				<path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+			  </svg>
+			</div> -->
+			<div class="flex-grow">
+			  <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Shooting Stars</h2>
+			  <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+			  <a class="mt-3 text-blue-500 inline-flex items-center">Learn More
+				<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+				  <path d="M5 12h14M12 5l7 7-7 7"></path>
+				</svg>
+			  </a>
+			</div>
+		  </div>
+		</div>
+		
+	  </div>
+	</div>
+  </section>
+
+<section class="text-gray-600 body-font relative">
+	<div class="absolute inset-0 bg-gray-300">
+		<iframe
+			width="100%"
+			height="100%"
+			frameborder="0"
+			marginheight="0"
+			marginwidth="0"
+			title="map"
+			scrolling="no"
+			src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
+			style="filter: grayscale(1) contrast(1.2) opacity(0.4);"
+		/>
+	</div>
+	<div class="container px-5 py-24 mx-auto flex">
+		<div
+			class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md"
+		>
+			<h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Contact us</h2>
+			<p class="leading-relaxed mb-5 text-gray-600">
+				Post-ironic portland shabby chic echo park, banjo fashion axe
+			</p>
+			<div class="relative mb-4">
+				<label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+				<input
+					type="email"
+					id="email"
+					name="email"
+					class="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+				/>
+			</div>
+			<div class="relative mb-4">
+				<label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+				<textarea
+					id="message"
+					name="message"
+					class="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+				/>
+			</div>
+			<button
+				class="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg"
+				>Button</button
+			>
+			<p class="text-xs text-gray-500 mt-3">
+				Chicharrones blog helvetica normcore iceland tousled brook viral artisan.
+			</p>
+		</div>
+	</div>
+</section>
